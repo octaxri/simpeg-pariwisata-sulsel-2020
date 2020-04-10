e57b9735440f5c398aecc9bf910505b019f23777
himawan94 <himawan94@gmail.com>
2020-04-10 13:39:48 +0800

    ubah icon tab halaman login dan penambahan optimasi print out sk mutasi

--- application/views/admin/data_mutasi/print_kelompokNew.php
+++ application/views/admin/data_mutasi/print_kelompokNew.php
@@ -1,96 +1,89 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Lampiran SK Mutasi Pelaksana</title>
+    <title>Lampiran SK Mutasi Pelaksana</title>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 
-<style type="text/css" >
-    html * {
-        font-family: Arial, Helvetica, sans-serif !important;
-        line-height: 1.5;
-    }
-	h1{
-		font-size: 1.5rem;
-		margin-bottom: 0px;
-	}
-	h2{
-		font-size: 1.2rem;
-		margin-bottom: 0px;
-	}
-	p, ol li{
-		font-size: 0.9rem;
-		margin-top: 0px;
-		margin-bottom: 0px;
-	}
-	.text-center{
-		text-align: center;
-	}
-	.c1{
-		margin-bottom: 0px;
-	}
-	.c2{
-		font-weight: 100;
-		margin-top: 0px;
-		letter-spacing: 10px;
-	}
-	.c3{
-		font-weight: bold;
-		width: 60%;
-		margin:auto;
-	}
-	.c3 > p {
-		margin-top: 0px;
-		margin-bottom: 0px;
-	}
-	.table {
-	  border-collapse: collapse;
-        border: 1px solid black;
-        font-size: 0.9rem;
-	}
+    <style type="text/css" >
+        html * {
+            font-family: Arial, Helvetica, sans-serif !important;
+            line-height: 1.5;
+        }
+        h1{
+            font-size: 1.5rem;
+            margin-bottom: 0px;
+        }
+        h2{
+            font-size: 1.2rem;
+            margin-bottom: 0px;
+        }
+        p, ol li{
+            font-size: 0.9rem;
+            margin-top: 0px;
+            margin-bottom: 0px;
+        }
+        .text-center{
+            text-align: center;
+        }
+        .c1{
+            margin-bottom: 0px;
+        }
+        .c2{
+            font-weight: 100;
+            margin-top: 0px;
+            letter-spacing: 10px;
+        }
+        .c3{
+            font-weight: bold;
+            width: 60%;
+            margin:auto;
+        }
+        .c3 > p {
+            margin-top: 0px;
+            margin-bottom: 0px;
+        }
+        .table {
+            border-collapse: collapse;
+            border: 1px solid black;
+            font-size: 0.9rem;
+        }
 
-	table td{
-		vertical-align:top;
-	}
+        table td{
+            vertical-align:top;
+        }
 
-    @page {
-        size: legal;
-        margin: 8%;
-    }
-    /*@media print {*/
-    /*    table.table tbody tr td:before,*/
-    /*    table.table tbody tr td:after {*/
-    /*        content: "";*/
-    /*        display: block;*/
-    /*    }*/
-    /*}*/
+        @page {
+            size: legal;
+            margin: 8%;
+        }
 
-    /*css style agar footernya nempel terus seperti aku dan kamu uncchhh*/
-    footer {
-         position: fixed;
-         height: 20px;
-         bottom: 0px;
-         left: 8%;
-         right:  8%;
-         margin-bottom: 0px;
-     }
-    body {
-        margin-bottom:8px;
-    }
+        /*css style agar footernya nempel terus seperti aku dan kamu uncchhh*/
+        footer {
+            position: fixed;
+            height: 20px;
+            bottom: 0px;
+            left: 8%;
+            right:  8%;
+            margin-bottom: 0px;
+        }
+        body {
+            margin-bottom:8px;
+        }
 
-</style>
+    </style>
 
 </head>
 <body>
 <div id="div">
     <div>
         <div class="text-center">
-        <img src="<?=assets_url('img').'pemprov-bnw.png'?>" style="width: 75px;">
-        <p style="margin: 0; padding: 0; font-size: 1rem;">PEMERINTAH PROPINSI SULAWESI SELATAN</p>
-        <p style="margin: 0; padding: 0; font-size: 1.4rem; font-weight: bold">DINAS KEBUDAYAAN DAN KEPARIWISATAAN</p>
-        <p style="margin: 0; padding: 0; font-size: 0.7rem">Gedung Mulo Jl. Sudirman No. 23 Telp. (0411) 878912 Fax 872314 MAKASSAR 90133</p>
-        <p style="margin: 0; padding: 0; font-size: 0.7rem">Homepage: www.Celebestourism.co.id e-mail: disbudpar.provsulsel@gmail.com</p>
-        <hr style=" border: 1px black solid; margin-bottom: 3px;">
-        <hr style=" border: 2px black solid; margin-top: 0;">
+            <img src="<?=assets_url('img').'pemprov-bnw.png'?>" style="width: 75px;">
+            <p style="margin: 0; padding: 0; font-size: 1rem;">PEMERINTAH PROPINSI SULAWESI SELATAN</p>
+            <p style="margin: 0; padding: 0; font-size: 1.4rem; font-weight: bold">DINAS KEBUDAYAAN DAN KEPARIWISATAAN</p>
+            <p style="margin: 0; padding: 0; font-size: 0.7rem">Gedung Mulo Jl. Sudirman No. 23 Telp. (0411) 878912 Fax 872314 MAKASSAR 90133</p>
+            <p style="margin: 0; padding: 0; font-size: 0.7rem">Homepage: www.Celebestourism.co.id e-mail: disbudpar.provsulsel@gmail.com</p>
+            <hr style=" border: 1px black solid; margin-bottom: 3px;">
+            <hr style=" border: 2px black solid; margin-top: 0;">
         </div>
 
         <div class="c3 text-center">
@@ -160,9 +153,9 @@
             <td width="20px;" style="color: white"></td>
         </tr>
         <?php
-            $tmt = $sk->tmt_sk;
-            $tmt = str_replace('/', '-', $tmt);
-            $tmt1 = date("Y-m-d", strtotime($tmt ));
+        $tmt = $sk->tmt_sk;
+        $tmt = str_replace('/', '-', $tmt);
+        $tmt1 = date("Y-m-d", strtotime($tmt ));
         ?>
         <tr>
             <td width="50px;" style="color: white"></td>
@@ -176,13 +169,13 @@
     <hr style="margin-bottom: 0px; border: 0px;">
     <table class="table" border="1">
         <thead style="border: 1px solid black">
-            <tr >
-                <th height="50px" style="vertical-align : middle;text-align:center; border: 1px solid black " > No.</th>
-                <th height="50px" width="32%" style="vertical-align : middle;text-align:center; border: 1px solid black" > Nama/NIP</th>
-                <th height="50px" width="18%" style="vertical-align : middle;text-align:center; border: 1px solid black" > Pangkat/Gol.</th>
-                <th height="50px" width="22%" style="vertical-align : middle;text-align:center; border: 1px solid black"> Jabatan Lama</th>
-                <th height="50px" width="22%" style="vertical-align : middle;text-align:center; border: 1px solid black"> Jabatan Baru</th>
-            </tr>
+        <tr >
+            <th height="50px" style="vertical-align : middle;text-align:center; border: 1px solid black " > No.</th>
+            <th height="50px" width="32%" style="vertical-align : middle;text-align:center; border: 1px solid black" > Nama/NIP</th>
+            <th height="50px" width="18%" style="vertical-align : middle;text-align:center; border: 1px solid black" > Pangkat/Gol.</th>
+            <th height="50px" width="22%" style="vertical-align : middle;text-align:center; border: 1px solid black"> Jabatan Lama</th>
+            <th height="50px" width="22%" style="vertical-align : middle;text-align:center; border: 1px solid black"> Jabatan Baru</th>
+        </tr>
         </thead>
         <tbody>
         <?php $i=1; foreach ($individu as $b): ?>
@@ -198,7 +191,7 @@
     </table>
 </div>
 <div>
-<br>
+    <br>
     <table style=" font-size: 0.9rem;">
         <tr>
             <td width="50px;" style="color: white"></td>
@@ -208,8 +201,8 @@
             <td width="20px;" style="color: white"></td>
         </tr>
     </table>
-<br>
-<br>
+    <br>
+    <br>
     <table style="width: 100%; font-size: 0.9rem;" class="text-center">
         <tr>
             <td width="50%">
@@ -237,25 +230,29 @@
 
     $(document).ready(function () {
 
-            var height = $("#div").height();
-            console.log(height);
-            if (height >= 952) {
-                if (height <= 1442) {
-                    $("#div").css('margin-bottom', '0px');
-                } else if ((height > 1442) && (height <= 1560)) {
-                    $("#div").css('margin-bottom', '260px');
-                } else if ((height > 1560) && (height <= 2066)) {
-                    $("#div").css('margin-bottom', '0px');
-                } else if ((height > 2066) && (height <= 2239)) {
-                    $("#div").css('margin-bottom', '260px');
-                }
-            }else if (height < 952) {
-                if (height <= 797) {
-                    $("#div").css('margin-bottom', '0px');
-                } else if (height > 797) {
-                    $("#div").css('margin-bottom', '260px');
-                }
+        var height = $("#div").height();
+        console.log(height);
+        if (height >= 952) {
+            if (height <= 1442) {
+                $("#div").css('margin-bottom', '0px');
+            } else if ((height > 1442) && (height <= 1560)) {
+                $("#div").css('margin-bottom', '260px');
+            } else if ((height > 1560) && (height <= 2066)) {
+                $("#div").css('margin-bottom', '0px');
+            } else if ((height > 2066) && (height <= 2239)) {
+                $("#div").css('margin-bottom', '260px');
+            } else if ((height > 2239) && (height <= 2648)) {
+                $("#div").css('margin-bottom', '0px');
+            } else if ((height > 2648) && (height <= 2677)) {
+                $("#div").css('margin-bottom', '260px');
             }
+        }else if (height < 952) {
+            if (height <= 797) {
+                $("#div").css('margin-bottom', '0px');
+            } else if (height > 797) {
+                $("#div").css('margin-bottom', '260px');
+            }
+        }
 
         window.print();
     });
